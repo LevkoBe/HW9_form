515a24042b6ad0e2b787f832448bae8999d02e44
LevkoBe <lbeniakh@kse.org.ua>
2023-10-12 20:40:12 +0300

    Not proper multiplications

--- scripts/functionality.js
+++ scripts/functionality.js
@@ -15,6 +15,37 @@ let getStringorial = (str) => {
   return str;
 };
 
+// Carrying
+let m = (a) => a;
+let mm = (a) => (b) => a * b;
+let mmm = (a) => (b) => (c) => a * b * c;
+let mmmm = (a) => (b) => (c) => (d) => a * b * c * d;
+
+function multiple(x) {
+  let product = x;
+
+  function next(y) {
+    product *= y;
+    return next;
+  }
+  next.toString = function () {
+    return product;
+  };
+
+  return next;
+}
+
+console.log(multiple(2)(3)(10).toString); //  60
+console.log(multiple(-1)(677)(568)(2).toString); //  -769072
+console.log(multiple(3).toString); //  3
+
+// Not carrying
+let multiply = (...args) => args.reduce((accumulator, currentValue) => accumulator * currentValue);
+
+console.log(multiply(2)(3)(10)); //  60
+console.log(multiply(-1)(677)(568)(2)); //  -769072
+console.log(multiply(3)); //  3
+
 console.log(getFactorial(0));
 console.log(getFactorial(1));
 console.log(getFactorial(2));
